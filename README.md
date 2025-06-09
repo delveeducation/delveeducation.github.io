80094c143b3bcb6a2e883794c26c10dda7733975
ferran23s <ferran23silva@gmail.com>
2025-06-09 11:55:02 -0400

    new web

--- README.md
+++
@@ -1,1 +0,0 @@
-# delveeducation.github.io
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("inscribirme").addEventListener("click", function () {
+  alert("Te has inscrito en el curso de Computer Vision");
+});
